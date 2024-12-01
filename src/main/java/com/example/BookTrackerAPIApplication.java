5daf65656de07671634e83c4078a2a8b07fc72c6
GorbunovIvan <110302058+GorbunovIvan@users.noreply.github.com>
2024-12-01 09:05:50 +0200

    init

---
+++ src/main/java/com/example/BookTrackerAPIApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookTrackerAPIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookTrackerAPIApplication.class, args);
+	}
+
+}
